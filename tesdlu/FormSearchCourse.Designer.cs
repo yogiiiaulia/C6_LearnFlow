a624695b6d9c4e5a2b7690aa858be3aef010fcf8
yogiiiaulia <yogiauliaardiano@gmail.com>
2026-04-16 10:07:07 +0700

    pembuatan pencarian course

--- tesdlu/FormSearchCourse.Designer.cs
+++ tesdlu/FormSearchCourse.Designer.cs
@@ -1,1 +1,84 @@
-﻿
+﻿namespace tesdlu
+{
+    partial class FormSearchCourse
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridView dgvCourses;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dgvCourses = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCourses)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.Location = new System.Drawing.Point(20, 20);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(175, 41);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Cari Kursus";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(20, 70);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(350, 22);
+            this.txtSearch.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(152)))), ((int)(((byte)(219)))));
+            this.btnSearch.ForeColor = System.Drawing.Color.White;
+            this.btnSearch.Location = new System.Drawing.Point(380, 68);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(100, 30);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Cari";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            // 
+            // dgvCourses
+            // 
+            this.dgvCourses.AllowUserToAddRows = false;
+            this.dgvCourses.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvCourses.ColumnHeadersHeight = 29;
+            this.dgvCourses.Location = new System.Drawing.Point(20, 120);
+            this.dgvCourses.Name = "dgvCourses";
+            this.dgvCourses.ReadOnly = true;
+            this.dgvCourses.RowHeadersWidth = 51;
+            this.dgvCourses.Size = new System.Drawing.Size(760, 350);
+            this.dgvCourses.TabIndex = 3;
+            // 
+            // FormSearchCourse
+            // 
+            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.dgvCourses);
+            this.Name = "FormSearchCourse";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cari Kursus";
+            this.Load += new System.EventHandler(this.FormSearchCourse_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCourses)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}

9ca392a0fc6c88577b08ef5b7e182fedeba2ea3d
yogiiiaulia <yogiauliaardiano@gmail.com>
2026-04-16 09:56:22 +0700

    mulai pembuatan Edit profile

--- tesdlu/FormEditProfil.cs
+++ tesdlu/FormEditProfil.cs
@@ -1,1 +1,77 @@
-﻿
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace tesdlu
+{
+    public partial class FormEditProfil : Form
+    {
+        private int userId;
+        private string currentFullName;
+        private SqlConnection con;
+
+        public FormEditProfil(int userId, string fullName)
+        {
+            InitializeComponent();
+            this.userId = userId;
+            this.currentFullName = fullName;
+            con = new SqlConnection(@"Data Source=LAPTOP-IUIDNP6D\YOGI;Initial Catalog=DBlearnFlow;Integrated Security=True");
+        }
+
+        private void FormEditProfil_Load(object sender, EventArgs e)
+        {
+            txtFullName.Text = currentFullName;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string newFullName = txtFullName.Text.Trim();
+            string newPassword = txtPassword.Text;
+            string confirmPassword = txtConfirmPassword.Text;
+
+            if (newFullName == "")
+            {
+                MessageBox.Show("Nama lengkap tidak boleh kosong!");
+                return;
+            }
+
+            if (newPassword != "" && newPassword != confirmPassword)
+            {
+                MessageBox.Show("Password dan konfirmasi password tidak cocok!");
+                return;
+            }
+
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("sp_UpdateProfile", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@idUser", userId);
+                cmd.Parameters.AddWithValue("@fullName", newFullName);
+                cmd.Parameters.AddWithValue("@password", newPassword == "" ? currentFullName : newPassword);
+
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    MessageBox.Show(dr["message"].ToString());
+                }
+                dr.Close();
+                con.Close();
+
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+                con.Close();
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}

277eda6306515c6680904277bf7bc5414e3c2713
yogiiiaulia <yogiauliaardiano@gmail.com>
2026-04-16 10:05:54 +0700

    mulai pembuatan form nilai

--- tesdlu/FormMyGrades.cs
+++ tesdlu/FormMyGrades.cs
@@ -1,1 +1,70 @@
-﻿
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace tesdlu
+{
+    public partial class FormMyGrades : Form
+    {
+        private int userId;
+
+        public FormMyGrades(int userId)
+        {
+            InitializeComponent();
+            this.userId = userId;
+            this.Load += FormMyGrades_Load;
+        }
+
+        private void FormMyGrades_Load(object sender, EventArgs e)
+        {
+            LoadGrades();
+        }
+
+        private void LoadGrades()
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-IUIDNP6D\YOGI;Initial Catalog=DBlearnFlow;Integrated Security=True"))
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("sp_ViewGrades", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idUser", userId);
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    dgvGrades.DataSource = dt;
+                    con.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void FormMyGrades_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormMyGrades
+            // 
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Name = "FormMyGrades";
+            this.Load += new System.EventHandler(this.FormMyGrades_Load_2);
+            this.ResumeLayout(false);
+
+        }
+
+        private void FormMyGrades_Load_2(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

1b9c807162ee070fb28bf4bd84167639a2d1e9ac
yogiiiaulia <yogiauliaardiano@gmail.com>
2026-04-16 09:52:54 +0700

    Add project files.

---
+++ tesdlu/FormLogin.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace tesdlu
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtusername = new System.Windows.Forms.Label();
+            this.txtpassword = new System.Windows.Forms.Label();
+            this.tbusername = new System.Windows.Forms.TextBox();
+            this.tbpassword = new System.Windows.Forms.TextBox();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.cbshowpassword = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(267, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(376, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LEARNFLOW SYSTEM";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(356, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(170, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Login Ke Sistem";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtusername
+            // 
+            this.txtusername.AutoSize = true;
+            this.txtusername.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.txtusername.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtusername.Location = new System.Drawing.Point(168, 175);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(87, 22);
+            this.txtusername.TabIndex = 2;
+            this.txtusername.Text = "Username";
+            this.txtusername.Click += new System.EventHandler(this.txtusername_Click);
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.AutoSize = true;
+            this.txtpassword.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.txtpassword.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpassword.Location = new System.Drawing.Point(171, 259);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(81, 22);
+            this.txtpassword.TabIndex = 3;
+            this.txtpassword.Text = "Password";
+            this.txtpassword.Click += new System.EventHandler(this.txtpassword_Click);
+            // 
+            // tbusername
+            // 
+            this.tbusername.Location = new System.Drawing.Point(342, 172);
+            this.tbusername.Name = "tbusername";
+            this.tbusername.Size = new System.Drawing.Size(241, 22);
+            this.tbusername.TabIndex = 4;
+            this.tbusername.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tbpassword
+            // 
+            this.tbpassword.Location = new System.Drawing.Point(342, 259);
+            this.tbpassword.Name = "tbpassword";
+            this.tbpassword.Size = new System.Drawing.Size(241, 22);
+            this.tbpassword.TabIndex = 5;
+            this.tbpassword.TextChanged += new System.EventHandler(this.tbpassword_TextChanged);
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.BackColor = System.Drawing.Color.SkyBlue;
+            this.btnlogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlogin.Location = new System.Drawing.Point(342, 391);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(241, 54);
+            this.btnlogin.TabIndex = 6;
+            this.btnlogin.Text = "LOGIN";
+            this.btnlogin.UseVisualStyleBackColor = false;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // cbshowpassword
+            // 
+            this.cbshowpassword.AutoSize = true;
+            this.cbshowpassword.Location = new System.Drawing.Point(637, 259);
+            this.cbshowpassword.Name = "cbshowpassword";
+            this.cbshowpassword.Size = new System.Drawing.Size(125, 20);
+            this.cbshowpassword.TabIndex = 7;
+            this.cbshowpassword.Text = "Show Password";
+            this.cbshowpassword.UseVisualStyleBackColor = true;
+            this.cbshowpassword.CheckedChanged += new System.EventHandler(this.cbshowpassword_CheckedChanged);
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(888, 507);
+            this.Controls.Add(this.cbshowpassword);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.tbpassword);
+            this.Controls.Add(this.tbusername);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtusername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormLogin";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label txtusername;
+        private System.Windows.Forms.Label txtpassword;
+        private System.Windows.Forms.TextBox tbusername;
+        private System.Windows.Forms.TextBox tbpassword;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.CheckBox cbshowpassword;
+    }
+}
+
---
+++ tesdlu/FormLogin.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace tesdlu
+{
+    public partial class FormLogin : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-IUIDNP6D\YOGI;Initial Catalog=DBlearnFlow;Integrated Security=True");
+        public FormLogin()
+        {
+
+            InitializeComponent();
+            TestConnection();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tbpassword.UseSystemPasswordChar = true;
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void TestConnection()
+        {
+            try
+            {
+                con.Open();
+                MessageBox.Show("Koneksi berhasil!");
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void txtusername_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtpassword_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbpassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbshowpassword_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbshowpassword.Checked)
+            {
+                tbpassword.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                tbpassword.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            string username = tbusername.Text.Trim();
+            string password = tbpassword.Text;
+
+            if (username == "" || password == "")
+            {
+                MessageBox.Show("Username dan Password tidak boleh kosong!");
+                return;
+            }
+
+            try
+            {
+                con.Open();
+
+                // Ambil juga idUser
+                SqlCommand cmd = new SqlCommand(@"
+            SELECT idUser, fullName, role 
+            FROM Users 
+            WHERE username = @username AND password = @password", con);
+
+                cmd.Parameters.AddWithValue("@username", username);
+                cmd.Parameters.AddWithValue("@password", password);
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    // Ambil userId sebagai int
+                    int userId = Convert.ToInt32(dr["idUser"]);
+                    string nama = dr["fullName"].ToString();
+                    string role = dr["role"].ToString();
+
+                    MessageBox.Show("Login BERHASIL!\nSelamat datang " + nama);
+
+                    // Kirim userId (int), bukan username (string)
+                    FormDashboard dashboard = new FormDashboard(userId, nama, role);
+                    dashboard.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Username atau Password salah!");
+                }
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+            }
+        }
+
+
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

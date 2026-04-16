2bb94b98dfc751367ef0b7d17ef2d429088548a3
yogiiiaulia <yogiauliaardiano@gmail.com>
2026-04-16 09:59:34 +0700

    selesai pembuatan input grade

--- tesdlu/FormInputGrade.Designer.cs
+++ tesdlu/FormInputGrade.Designer.cs
@@ -1,1 +1,84 @@
-﻿
+﻿namespace tesdlu
+{
+    partial class FormInputGrade
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.ComboBox cmbCourse;
+        private System.Windows.Forms.DataGridView dgvStudents;
+        private System.Windows.Forms.Button btnSave;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.cmbCourse = new System.Windows.Forms.ComboBox();
+            this.dgvStudents = new System.Windows.Forms.DataGridView();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.Location = new System.Drawing.Point(20, 20);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(170, 41);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Input Nilai";
+            // 
+            // cmbCourse
+            // 
+            this.cmbCourse.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCourse.Location = new System.Drawing.Point(20, 70);
+            this.cmbCourse.Name = "cmbCourse";
+            this.cmbCourse.Size = new System.Drawing.Size(300, 24);
+            this.cmbCourse.TabIndex = 1;
+            // 
+            // dgvStudents
+            // 
+            this.dgvStudents.AllowUserToAddRows = false;
+            this.dgvStudents.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvStudents.ColumnHeadersHeight = 29;
+            this.dgvStudents.Location = new System.Drawing.Point(20, 110);
+            this.dgvStudents.Name = "dgvStudents";
+            this.dgvStudents.RowHeadersWidth = 51;
+            this.dgvStudents.Size = new System.Drawing.Size(760, 300);
+            this.dgvStudents.TabIndex = 2;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(680, 430);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(100, 35);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Simpan Nilai";
+            this.btnSave.UseVisualStyleBackColor = false;
+            // 
+            // FormInputGrade
+            // 
+            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.cmbCourse);
+            this.Controls.Add(this.dgvStudents);
+            this.Controls.Add(this.btnSave);
+            this.Name = "FormInputGrade";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Input Nilai";
+            this.Load += new System.EventHandler(this.FormInputGrade_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}

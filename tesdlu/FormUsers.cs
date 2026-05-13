da0f364084ebce80649c6513156e77d9e0e8456c
yogiiiaulia <yogiauliaardiano@gmail.com>
2026-05-13 23:56:47 +0700

    kasi keterangan kolom

--- tesdlu/FormUsers.cs
+++ tesdlu/FormUsers.cs
@@ -9,6 +9,7 @@ namespace tesdlu
     {
         private SqlConnection con;
         private int selectedUserId = -1;
+        private bool isUpdatingPasswordField = false;
 
         public FormUsers()
         {
@@ -20,6 +21,14 @@ namespace tesdlu
             btnDelete.Click += BtnDelete_Click;
             btnRefresh.Click += BtnRefresh_Click;
             dgvUsers.CellClick += DgvUsers_CellClick;
+
+            // Placeholder events
+            txtUsername.GotFocus += TxtUsername_GotFocus;
+            txtUsername.LostFocus += TxtUsername_LostFocus;
+            txtFullName.GotFocus += TxtFullName_GotFocus;
+            txtFullName.LostFocus += TxtFullName_LostFocus;
+            txtPassword.GotFocus += TxtPassword_GotFocus;
+            txtPassword.LostFocus += TxtPassword_LostFocus;
         }
 
         private void FormUsers_Load(object sender, EventArgs e)
@@ -47,7 +56,9 @@ namespace tesdlu
 
         private void BtnAdd_Click(object sender, EventArgs e)
         {
-            if (txtUsername.Text == "" || txtFullName.Text == "" || txtPassword.Text == "")
+            if (txtUsername.Text == "" || txtUsername.Text == "Username" || 
+                txtFullName.Text == "" || txtFullName.Text == "Full Name" || 
+                txtPassword.Text == "" || txtPassword.Text == "Password")
             {
                 MessageBox.Show("Isi semua field!");
                 return;
@@ -56,9 +67,10 @@ namespace tesdlu
             try
             {
                 con.Open();
+                string hashedPassword = BCrypt.Net.BCrypt.HashPassword(txtPassword.Text);
                 SqlCommand cmd = new SqlCommand("INSERT INTO Users (username, password, fullName, role) VALUES (@u, @p, @f, @r)", con);
                 cmd.Parameters.AddWithValue("@u", txtUsername.Text);
-                cmd.Parameters.AddWithValue("@p", txtPassword.Text);
+                cmd.Parameters.AddWithValue("@p", hashedPassword);
                 cmd.Parameters.AddWithValue("@f", txtFullName.Text);
                 cmd.Parameters.AddWithValue("@r", cmbRole.Text);
                 cmd.ExecuteNonQuery();
@@ -86,9 +98,10 @@ namespace tesdlu
             try
             {
                 con.Open();
+                string hashedPassword = BCrypt.Net.BCrypt.HashPassword(txtPassword.Text);
                 SqlCommand cmd = new SqlCommand("UPDATE Users SET username=@u, fullName=@f, role=@r, password=@p WHERE idUser=@id", con);
                 cmd.Parameters.AddWithValue("@u", txtUsername.Text);
-                cmd.Parameters.AddWithValue("@p", txtPassword.Text);
+                cmd.Parameters.AddWithValue("@p", hashedPassword);
                 cmd.Parameters.AddWithValue("@f", txtFullName.Text);
                 cmd.Parameters.AddWithValue("@r", cmbRole.Text);
                 cmd.Parameters.AddWithValue("@id", selectedUserId);
@@ -150,24 +163,95 @@ namespace tesdlu
                 DataGridViewRow row = dgvUsers.Rows[e.RowIndex];
                 selectedUserId = Convert.ToInt32(row.Cells["idUser"].Value);
                 txtUsername.Text = row.Cells["username"].Value.ToString();
+                txtUsername.ForeColor = System.Drawing.Color.Black;
                 txtFullName.Text = row.Cells["fullName"].Value.ToString();
+                txtFullName.ForeColor = System.Drawing.Color.Black;
                 cmbRole.Text = row.Cells["role"].Value.ToString();
                 txtPassword.Text = "";
+                txtPassword.ForeColor = System.Drawing.Color.Black;
+                txtPassword.UseSystemPasswordChar = false;
             }
         }
 
         private void ClearForm()
         {
             selectedUserId = -1;
-            txtUsername.Text = "";
-            txtFullName.Text = "";
-            txtPassword.Text = "";
+            txtUsername.Text = "Username";
+            txtUsername.ForeColor = System.Drawing.Color.Gray;
+            txtFullName.Text = "Full Name";
+            txtFullName.ForeColor = System.Drawing.Color.Gray;
+            txtPassword.Text = "Password";
+            txtPassword.ForeColor = System.Drawing.Color.Gray;
+            txtPassword.UseSystemPasswordChar = false;
             cmbRole.Text = "Student";
         }
 
-        private void FormUsers_Load_1(object sender, EventArgs e)
+        private void TxtUsername_GotFocus(object sender, EventArgs e)
         {
+            if (txtUsername.Text == "Username")
+            {
+                txtUsername.Text = "";
+                txtUsername.ForeColor = System.Drawing.Color.Black;
+            }
+        }
 
+        private void TxtUsername_LostFocus(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "")
+            {
+                txtUsername.Text = "Username";
+                txtUsername.ForeColor = System.Drawing.Color.Gray;
+            }
+        }
+
+        private void TxtFullName_GotFocus(object sender, EventArgs e)
+        {
+            if (txtFullName.Text == "Full Name")
+            {
+                txtFullName.Text = "";
+                txtFullName.ForeColor = System.Drawing.Color.Black;
+            }
+        }
+
+        private void TxtFullName_LostFocus(object sender, EventArgs e)
+        {
+            if (txtFullName.Text == "")
+            {
+                txtFullName.Text = "Full Name";
+                txtFullName.ForeColor = System.Drawing.Color.Gray;
+            }
+        }
+
+        private void TxtPassword_GotFocus(object sender, EventArgs e)
+        {
+            if (isUpdatingPasswordField) return;
+            
+            isUpdatingPasswordField = true;
+            if (txtPassword.Text == "Password")
+            {
+                txtPassword.Text = "";
+                txtPassword.ForeColor = System.Drawing.Color.Black;
+                txtPassword.UseSystemPasswordChar = true;   
+            }
+            isUpdatingPasswordField = false;
+        }
+
+        private void TxtPassword_LostFocus(object sender, EventArgs e)
+        {
+            if (isUpdatingPasswordField) return;
+
+            isUpdatingPasswordField = true;
+            if (txtPassword.Text == "" && selectedUserId == -1)
+            {
+                txtPassword.Text = "Password";
+                txtPassword.ForeColor = System.Drawing.Color.Gray;
+                txtPassword.UseSystemPasswordChar = false;
+            }
+            else if (txtPassword.Text != "" && txtPassword.Text != "Password")
+            {
+                txtPassword.ForeColor = System.Drawing.Color.Black;
+            }
+            isUpdatingPasswordField = false;
         }
     }
 }
